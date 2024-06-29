ee6fa795fdc4fe2861c4f42e1cc56a6621cb66cb
Leonid Poluiakhtov <len4ik-0123@yandex.ru>
2024-06-29 14:51:54 +0400

    new class Box

---
+++ src/Box.java
@@ -0,0 +1,14 @@
+public class Box {
+    private int size;
+
+    public Box() {
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
